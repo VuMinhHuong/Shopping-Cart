a17e6a92731d0782ed0373889b7ec1d9d345936d
HuongVM <vuminhhuong.vn@gmail.com>
2023-02-17 17:47:04 +0700

    17022023-1

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,16 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import "./Product.scss";
 
 const Product = () => {
 
     const [qty, setQty] = useState(1);
 
   return (
-    <div>
-      <div className="img"><img src="../../public/img/iphone.png" alt="iphone" /></div>
+    <div className='product'>
+
+      <div className="img"><img src="https://cdn.tgdd.vn/Products/Images/44/239552/Slider/vi-vn-apple-macbook-air-m1-2020-z124000de-8.jpg" alt="iphone" /></div>
+      
       <div className="des">
-        <div className="des-title">Iphone</div>
+        <div className="des-title">macbook</div>
         <div className="title-dest">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
       </div>
+      
       <div className="action">
         <div className="price">1200$</div>
         <div className="qty">
@@ -20,6 +24,7 @@ const Product = () => {
         </div>
         <button className='add-to-card'>Add to card</button>
       </div>
+
     </div>
   )
 }
--- src/pages/Shopping.jsx
+++ src/pages/Shopping.jsx
@@ -1,55 +1,31 @@
 import React from 'react'
-import "./Shopping.scss"
 import Product from '../components/Product'
-import logo from "../assets/img/Logo-Rikkei.png"
-import samsung from "../assets/img/samsung.png"
-import pad from "../assets/img/ipad.png"
-import iphone from "../assets/img/iphone.png"
-import macbook from "../assets/img/macbook.png"
+import "./Shopping.scss"
+// import Product from '../components/Product'
+// import logo from "/img/Logo-Rikkei.png"
+// import samsung from "../assets/img/samsung.png"
+// import pad from "../assets/img/ipad.png"
+// import iphone from "../assets/img/iphone.png"
+// import macbook from "../assets/img/macbook.png"
 
 const Shopping = () => {
-    let data_product = [
-        {
-            id: 1,
-            img: {samsung},
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            price: 1200
-        }, 
-        {
-            id: 2,
-            img: {pad},
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            price: 2200
-        }, 
-        {
-            id: 3,
-            img: {iphone},
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            price: 200
-        }, 
-        {
-            id: 4,
-            img: {macbook},
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            price: 3500
-        }, 
-        
-    ]
   return (
     <div className='shopping'>
-      <div className="logo"><img src={logo} alt="logo" /></div>
+      <div className="logo"><img src="https://rikkei.edu.vn/wp-content/uploads/2022/04/Logo-Rikkei.png" alt="logo" /></div>
       <div className="title">PROJECT - SHOPPING CART</div>
       
-      <div className="list-product">
-        <div className="list-product-title">List Products</div>
-        <div className="list-product-product">
-            {data_product.map((data, index) => {
+      <div className="container">
+        <div className="list-product">
+            <div className="list-product-title">List Products</div>
+            <div className="list-product-product">
+                <Product/>
+            </div>
+        </div>
 
-            })}
+        <div className="cart">
+        <div className="cart-product-title">Cart</div>
         </div>
       </div>
-
-      <div className="cart">Cart</div>
 
     </div>
   )
